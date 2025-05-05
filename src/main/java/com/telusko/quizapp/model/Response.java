eeac0b521353b3f78dbae87a5b715fd1d64486b8
nandini <nandinimaurya04@gmail.com>
2025-05-05 17:54:12 +0530

    Initial Commit

---
+++ src/main/java/com/telusko/quizapp/model/Response.java
@@ -0,0 +1,28 @@
+package com.telusko.quizapp.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Response {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    private String response;
+}
